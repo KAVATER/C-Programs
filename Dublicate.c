cf3ce93c79eb40b540353abc6133420bb057639a
KAVATER <kushpetwal13@gmail.com>
2025-06-07 08:45:24 +0530

    This program is to find the dublicate characters in a word

---
+++ Dublicate.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_CHAR 256
+
+int main()
+{
+    char str[100];
+    int count[MAX_CHAR] = {0};
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        count[(unsigned char)str[i]]++;
+    }
+
+    printf("Duplicate characters are: ");
+
+    int found = 0;
+    for (int i = 0; i < MAX_CHAR; i++) {
+        if (count[i] > 1) {
+            printf("%c ", i);
+            found++;
+        }
+    }
+
+    if (!found) {
+        printf("None");
+    }
+
+    printf("\n");
+    return 0;
+}
