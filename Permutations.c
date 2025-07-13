980915a84d5295f835060f605ca763dd200d69ef
Kushagra Petwal <kushpetwal13@gmail.com>
2025-07-13 18:22:37 +0530

    Finding permutations of a string.

--- Permutations.c
+++ Permutations.c
@@ -1,27 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-void reverse(char *left , char *right)
+void swap(char *x, char *y)
 {
-    if(left >= right)
-    {
-      return;
-    }
-    char temp;
-    temp = *left;
-    *left = *right;
-    *right = temp; 
-    reverse(left++, right++);
+    char temp = *x;
+    *x = *y;
+    *y = temp;
 }
+
+int shouldSwap(char *str, int start, int curr)
+{
+    for (int i = start; i < curr; i++) {
+        if (str[i] == str[curr]) {
+            return 0; // already swapped
+        }
+    }
+    return 1;
+}
+
+void permute(char *a, int i, int n)
+{
+    if (i == n) {
+        printf("%s\n", a);
+    } else {
+        for (int j = i; j <= n; j++) {
+            if (shouldSwap(a, i, j)) {
+                swap(&a[i], &a[j]);
+                permute(a, i + 1, n);
+                swap(&a[i], &a[j]); // backtrack
+            }
+        }
+    }
+}
+
 int main()
 {
-    char str[100];
-    printf("Enter the string.\n");
-    fgets(str,sizeof(str),stdin);
-    str[strcspn(str,"\n")] = '\0';
-
-    reverse(str, str + strlen(str)-1 );
-    printf("Reverse string is:\n");
-    printf("%s",str);
+    char a[20];
+    printf("Enter a string: ");
+    scanf("%s", a);
+    int n = strlen(a);
+    printf("Permutations:\n");
+    permute(a, 0, n - 1);
+    return 0;
 }
